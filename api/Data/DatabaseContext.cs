ace9b82c4fc5961522329c9c9451925dc9304d9e
Kalman Heyn <kalheyn@gmail.com>
2019-04-05 14:22:52 -0400

    Added EF core and configured to use Postgres

--- api/Data/DatabaseContext.cs
+++ api/Data/DatabaseContext.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using Salarylookup.Api.Models;
 
-namespace Salarylookup.Api.Models
+namespace Salarylookup.Api.Data
 {
     public class DatabaseContext : DbContext
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {}
+
+        protected override void OnModelCreating(ModelBuilder builder) => base.OnModelCreating(builder);
 
         public DbSet<Salary> Salaries { get; set; }
     }
