f0a8d9fe6d7511597f50b25e6cd1403199eedff2
Kalman Heyn <kalheyn@gmail.com>
2019-04-14 17:56:21 -0400

    update salariesURL

--- web/src/app/salaries/salaries.service.ts
+++ web/src/app/salaries/salaries.service.ts
@@ -16,7 +16,7 @@ export class SalariesService {
  * @param operation - name of the operation that failed
  * @param result - optional value to return as the observable result
  */
-  private salariesUrl = 'https://localhost:5001/api/search';
+  private salariesUrl = 'https://salarysearch.azurewebsites.net/api/search';
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send the error to remote logging infrastructure
