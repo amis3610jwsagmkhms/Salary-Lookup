1608e6a039fb68968754c0504684d7e5f3fbf0a7
Kalman Heyn <kalheyn@gmail.com>
2019-04-15 19:13:08 -0400

    cors

--- api/Startup.cs
+++ api/Startup.cs
@@ -27,7 +27,7 @@ namespace Salarylookup.Api
             {
                 options.AddPolicy("CorsPolicy", builder =>
                 {
-                    builder.WithOrigins("http://localhost:4200")
+                    builder.WithOrigins("http://kalheyn.z19.web.core.windows.net/")
                     .AllowAnyMethod()
                     .AllowAnyHeader();
                 });
